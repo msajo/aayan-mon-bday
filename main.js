7570670bc7690108c57952a03235c234d665c9b7
COOL- <87934102+msajo@users.noreply.github.com>
2023-01-20 17:58:57 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ var canvas = new fabric.Canvas('myCanvas');
 
 function new_image()
 {
-    fabric.Image.fromURL("BirthdayImage.jpeg",function (Img) {
+    fabric.Image.fromURL("BirthdayImage.png",function (Img) {
             block_image_object = Img;
-            block_image_object.scaleToWidth(700);
-            block_image_object.scaleToHeight(510);
+            block_image_object.scaleToWidth(800);
+            block_image_object.scaleToHeight(800);
             block_image_object.set({
                 top:0,
                 left:0
